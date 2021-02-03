20ba977775210e8893b1053a19ce00f297dd0cf4
40171936 <585-40171936@users.noreply.gitlab2.eeecs.qub.ac.uk>
2021-02-03 14:29:04 +0000

    compare fsca and opfs

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ def write_pca_analysis(mat_size, pca_duration):
 def main():
     print("main started")
 
-    start_time = time.time()
+    
     # do pca analysis
     square_mat = get_matrix(True)
-    # not_square_mat = get_matrix(False)
+    not_square_mat = get_matrix(False)
 
-    
-    #pca_results = []
-    S, M, VarEx, compId = do_opfs(square_mat[6])
-    print(S)
-    print(VarEx)
-    print(M)
-    print(compId)
+    # comparison of fsca and opfs
+
+    # square matrices
+    #for i in range(start=0, len(square_mat)):
+
+    start_time = time.time()
+
+    S_fsca, M_fsca, VarEx_fsca, comp_Id_fsca = do_opfs(square_mat[4])
+    print(VarEx_fsca)
+    print(comp_Id_fsca)
+    #    S_opfs, M_opfs, VarEx_opfs, compId_opfs = do_opfs(square_mat[i])
+
       
     duration = time.time() - start_time
     #print(results)
@@ -39,7 +44,7 @@ if __name__ == "__main__":
         from algorithms.opfs import do_opfs
     else:
         from .generators.matrix_generator import get_matrix
-        from ..helpers.algorithms.pca import pca_nipals
+        from .helpers.algorithms.pca import pca_nipals
         from .algorithms.fsca import do_fsca
         from .algorithms.opfs import do_opfs
     main()
