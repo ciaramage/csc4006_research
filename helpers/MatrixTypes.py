62ba92e689daf03f634328fb71648591e20671e6
40171936 <585-40171936@users.noreply.gitlab2.eeecs.qub.ac.uk>
2021-02-08 20:13:24 +0000

    refactored matric generator script. now includes independent and zero mean matrices

--- helpers/MatrixTypes.py
+++ helpers/MatrixTypes.py
@@ -3,5 +3,7 @@ from enum import Enum
 class MatrixTypes(Enum):
     INDEPENDENT = '1' # linearly independent
     ZERO_MEAN = '2' # zero mean columns
-    CORRELATION = '3' #correlation matrix
     PARTIALLY_INDEPENDENT = '4' # first columns independent - the other columns are functions of the first
+    ZERO_MEAN_CORR = '5' # correlation matrix from zero mean columns
+    INDEPENDENT_CORR ='6' # correlation matrix from independent
+    PARTIALLY_INDEPENDENT_CORR = '7' # correlation matrix from partially independet
