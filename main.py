cea469416f61e462e8a0f8254b0dc63933ac90b8
Ciara Magee, Student Number: 40171936 <cmagee609@qub.ac.uk>
2021-07-18 22:17:44 +0100

    refactored main.py

--- main.py
+++ main.py
@@ -4,41 +4,25 @@ import numpy as np
 
 def main():
     print("main started")
+    #x, d = random_duration('fsca')
+    #plot_random_times()
+    #randomDataInfo()
     #realDataInfo()
-    Nc1 = 8
-    #mat = np.loadtxt('data/X50sites.txt', delimiter=',')
 
-    """"     sizes = [(500, 50), (500, 100), (500,150), (500,200), (500,250),(500,300), (500,350), (500,400), (500,450), (500,500)]
-    
-    for i in range(len(sizes)): """
-    results = []
-    mat = read_matrix('data/realData/forestFires.txt')
+    #plot_real('glg','Xpitprops')
 
-    start_fsca = time.time()
-    _, _, varEx_fsca, compID_fsca = UFS(mat, Nc)
-    duration_fsca = time.time() - start_fsca
-    results.append(['ufs', varEx_fsca, compID_fsca, duration_fsca])
+    real_results('opfs', 'breastCancerDiagnosis', 0.5)
 
-    for result in results:
-        print(result)
-    
-    get_random_duration()
+    #real_sg_compare_results('Xpitprops', [0.1,0.3,0.5,0.7,0.9])
 
-    """ start_ufs = time.time()
-    _, _, varEx_ufs, compID_ufs = lazy_greedy_UFS(mat, 8)
-    duration_ufs = time.time() - start_ufs
-    results.append(['ufs lg', varEx_ufs, compID_ufs, duration_ufs])
+    #plot_real_sg_compare('Xpitprops', [0.1,0.3,0.5,0.7,0.9])
 
-    start_fsca_lg = time.time()
-    _,_, varEx_fsca_lg, compID_fsca_lg = stochastic_ufs(mat, 8, 0.3)
-    duration_fsca_lg = time.time() - start_fsca_lg
-    results.append(['ufs sg', varEx_fsca_lg, compID_fsca_lg, duration_fsca_lg]) """
+    """ sizes = [(50, 20), (100,20), (150,20), (200,20), (250,20), (300,20), (350,20), (400, 20), (450,20), (500,20)]
 
-    """ for i in range(len(sizes)):
+    for i in range(len(sizes)):
         mat = get_matrix(sizes[i])
-        write_matrix('data/randomData/t{0}.txt'.format(i+1), mat)
+        write_matrix('data/randomData/t{0}.txt'.format(i+1), mat) """
 
-    randomDataInfo() """
 
 if __name__ == "__main__":
     if __package__ is None:
@@ -46,27 +30,10 @@ if __name__ == "__main__":
         from os import path
         sys.path.append(path.dirname(path.abspath(__file__)))
         from helpers.matrix_in_out import *
-        from helpers.matrix_generator import get_matrix
-        #from helpers.toLatex import realDataInfo, randomDataInfo
-        from algorithms.fsca import fsca
-        from algorithms.opfs import opfs
-        from algorithms.ufs import ufs
-        from algorithms.fsca_lazy_greedy import fsca_lazy_greedy
-        from algorithms.ufs_lazy_greedy import ufs_lazy_greedy
-        from algorithms.fsca_stochastic_greedy import fsca_stochastic_greedy_orthogonal    
-        from algorithms.fsca_stochastic_def import fsca_stochastic_greedy_deflation
-        from algorithms.opfs_stochastic_greedy import opfs_stochastic_greedy_orthogonal   
-        from algorithms.opfs_stochastic_greedy_deflation import opfs_stochastic_greedy_deflation
-        from algorithms.ufs_stochastic_greedy import ufs_stochastic_greedy
+        #from helpers.matrix_generator import get_matrix
+        from helpers.toLatex import *
+        from helpers.experiment_results import *
     else:
-        from .algorithms.ufs import UFS
-        from .algorithms.fsca import FSCA
-        from .algorithms.opfs import OPFS
-        from .algorithms.fsca_lazy_greedy import lazy_greedy_FSCA
-        from .algorithms.ufs_lazy_greedy import UFS_new
-        from .algorithms.fsca_stochastic_greedy import STOCHASTIC_FSCA_ORT
-        from .algorithms.fsca_stochastic_def import STOCHASTIC_FSCA_DEF
-        from .algorithms.opfs_stochastic_greedy import STOCHASTIC_OPFS
-        from .algorithms.opfs_stochastic_greedy_deflation import STOCHASTIC_OPFS_DEF
-        from .algorithms.ufs_stochastic_greedy import STOCHASTIC_UFS
+        from .helpers.experiment_results import *
+
     main()
